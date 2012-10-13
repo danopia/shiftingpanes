e75ba0664c59249ae0b7636c608ec7743e3ae7eb
Daniel Lamando <me@danopia.net>
2012-10-12 21:29:26 -0400

    Added a NavPane. Slightly better demo.

--- panes.js
+++ panes.js
@@ -1,6 +1,6 @@
 $.PaneView = function (selector) {
   this.$dom = $($(selector)[0]);
-  this.$dom.css({position: 'relative', 'overflow-x': 'hidden'});
+  this.$dom.css({position: 'relative', 'overflow-x': 'visible'});
   this.stack = [];
 };
 
@@ -8,19 +8,15 @@ $.PaneView.prototype.last = function (offset) {
   return this.stack[this.stack.length - offset];
 };
 
-$.PaneView.prototype.pushPane = function (title) {
-  var pane = new $.PaneView.Pane(title);
+$.PaneView.prototype.pushPane = function (pane) {
   this.$dom.append(pane.$dom);
   this.stack.push(pane);
   
   if (this.stack.length == 1) {
     this.last(1).$dom.animate({left: 0,   right: 0});
   } else if (this.stack.length >= 2) {
-    this.last(1).$dom.animate({left: 200, right: 0});
-    this.last(2).$dom.animate({left: 0,   right: 600});
-    
-    if (this.stack.length > 2)
-      this.last(3).$dom.animate({left: -200, right: 800}); // TODO: and remove from DOM
+    this.last(1).$dom.animate({left: '20%', right: 0});
+    this.last(2).$dom.animate({left: 0,   right: '80%'});
   };
   
   return pane;
@@ -28,14 +24,13 @@ $.PaneView.prototype.pushPane = function (title) {
 
 $.PaneView.prototype.popPane = function () {
   if (this.stack.length == 1) {
-    this.last(1).$dom.animate({left: 800, right: 0}); // TODO: and remove from DOM
+    this.last(1).$dom.animate({left: '100%', right: 0}); // TODO: and remove from DOM
   } else if (this.stack.length == 2) {
-    this.last(1).$dom.animate({left: 800, right: 0}); // TODO: and remove from DOM
+    this.last(1).$dom.animate({left: '100%', right: 0}); // TODO: and remove from DOM
     this.last(2).$dom.animate({left: 0,   right: 0});
   } else if (this.stack.length >= 3) {
-    this.last(1).$dom.animate({left: 800, right: 0}); // TODO: and remove from DOM
-    this.last(2).$dom.animate({left: 200, right: 0});
-    this.last(3).$dom.animate({left: 0,   right: 600}); // TODO: and remove from DOM
+    this.last(1).$dom.animate({left: '100%', right: 0}); // TODO: and remove from DOM
+    this.last(2).$dom.animate({left: '20%', right: 0});
   };
   
   this.stack.pop();
@@ -44,15 +39,38 @@ $.PaneView.prototype.popPane = function () {
 
 $.PaneView.Pane = function (title) {
   this.$dom = $('<section/>', {'class': 'pane'});
-  this.$dom.css({position: 'absolute', left: '100%', right: 0, top: 0, bottom: 0});
+  this.$dom.css({position: 'absolute', left: '100%', right: 0, top: 0, bottom: 0, 'overflow-x': 'hidden'});
+  
+  this.title = title;
+  this.$title = $('<h3/>', {text: title}).appendTo(this.$dom);
   
   this.$inner = $('<div/>').appendTo(this.$dom);
   this.$inner.css({width: 600, 'overflow-x': 'hidden'});
-  
-  this.title = title;
 };
 
 $.PaneView.Pane.prototype.text = function (text) {
   this.$inner.text(text);
+};
+
+
+$.PaneView.NavPane = function (title) {
+  this.$dom = $('<section/>', {'class': 'nav pane'});
+  this.$dom.css({position: 'absolute', left: '100%', right: 0, top: 0, bottom: 0, 'overflow-x': 'hidden'});
+  
+  this.title = title;
+  this.$title = $('<h3/>', {text: title}).appendTo(this.$dom);
+  
+  this.$inner = $('<div/>').appendTo(this.$dom);
+  this.$inner.css({width: 600, 'overflow-x': 'hidden'});
+  
+  this.$ul = $('<ul/>').appendTo(this.$inner);
+};
+
+$.PaneView.NavPane.prototype.add = function (item, handler) {
+  var $a = $('<a/>', {text: item, href: '#'});
+  $a.on('click', handler);
+  
+  var $li = $('<li/>').append($a);
+  $li.appendTo(this.$ul);
 };
 
